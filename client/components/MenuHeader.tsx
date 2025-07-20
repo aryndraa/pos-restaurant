480d94d3b611cf8178647f69d34c3855d2a68d45
aryndraa <aryacoder1102@gmail.com>
2025-07-20 23:18:49 +0800

    menu page title

--- client/components/MenuHeader.tsx
+++ client/components/MenuHeader.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchModal from "./SearchModal";
 import CategoryNav from "./CategoryNav";
+import Link from "next/link";
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function MenuHeader() {
   return (
-    <div className="p-4 pb-2 fixed top-0 left-0 right-0 bg-background">
+    <div className="py-4 pb-2 flex flex-col gap-3  bg-background sticky top-0">
       <div className="bg-white flex items-center px-4 rounded-full py-3 gap-1 ">
         <SearchModal />
         <CategoryNav />
