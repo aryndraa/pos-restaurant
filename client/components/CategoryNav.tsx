f3e0ed55b2dfc07e37be94962237295314c8d6a9
aryndraa <aryacoder1102@gmail.com>
2025-07-23 18:56:42 +0800

    set id in category nav

--- client/components/CategoryNav.tsx
+++ client/components/CategoryNav.tsx
@@ -1,33 +1,63 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { useFoodCategory } from "@/lib/contexts/FoodCategoryContext";
 import { Categories } from "@/dummy/Categories";
+import SwiperCore from "swiper";
 
 export default function CategoryNav() {
-  const { setId } = useFoodCategory();
+  const { setId, id } = useFoodCategory();
+  const swiperRef = useRef<SwiperCore>(null);
+
+  useEffect(() => {
+    const index = Categories.findIndex((cat) => cat.id === id);
+    if (swiperRef.current && index !== -1) {
+      swiperRef.current.slideTo(index, 300); // 300ms = smooth transition
+    }
+  }, [id]);
 
   return (
     <div className="w-full overflow-x-hidden">
       <Swiper
         spaceBetween={10}
         slidesPerView={"auto"}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
         className="!overflow-visible"
       >
-        <SwiperSlide className="!w-fit flex-shrink-0 px-4 py-2  bg-primary/10 rounded-full">
-          <div className="flex items-center w-full gap-2 rounded-lg ">
-            <span>🍔</span>
-            <p className="font-bold text-primary">Pizza</p>
-          </div>
+        <SwiperSlide
+          className={`!w-fit flex-shrink-0 px-4 py-2 rounded-full ${
+            id === 0 && "bg-primary/10"
+          }`}
+        >
+          <button
+            onClick={() => setId(0)}
+            className="flex items-center w-full gap-2 rounded-lg "
+          >
+            <p className={`font-bold ${id === 0 && "text-primary"}`}>All</p>
+          </button>
         </SwiperSlide>
-        {Categories.map((_, idx) => (
-          <SwiperSlide key={idx} className="!w-fit  flex-shrink-0 px-4 py-2 ">
-            <div className="flex items-center w-full gap-2  rounded-lg">
-              <span>🍔</span>
-              <p className="font-bold">Pizza</p>
-            </div>
+        {Categories.map((category, idx) => (
+          <SwiperSlide
+            key={idx}
+            className={`!w-fit flex-shrink-0 px-4 py-2 rounded-full ${
+              id === category.id && "bg-primary/10"
+            }`}
+          >
+            <button
+              onClick={() => setId(category.id)}
+              className="flex items-center w-full gap-2  rounded-lg"
+            >
+              <span>{category.emoji}</span>
+              <p
+                className={`font-bold ${id === category.id && "text-primary"}`}
+              >
+                {category.name}
+              </p>
+            </button>
           </SwiperSlide>
         ))}
       </Swiper>
--- client/dummy/Food.ts
+++ client/dummy/Food.ts
@@ -30,14 +30,14 @@ export const Foods: Food[] = [
     price: 25000,
   },
   {
-    id: 1,
-    name: "Nigiri Sushi",
+    id: 2,
+    name: "Maki Sushi",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
     categoryId: 1,
     image:
-      "https://images.unsplash.com/photo-1617196035154-1e7e6e28b0db?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    rating: 5.6,
-    price: 25000,
+      "https://images.unsplash.com/photo-1617196034738-26c5f7c977ce?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    rating: 4.8,
+    price: 30000,
   },
 ];
